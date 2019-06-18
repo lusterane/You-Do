0412b31a2fb77b0c585fd62de5849dc4bd457d30
Toby Chow <tobychow98@gmail.com>
2019-06-18 12:20:34 -0500

    Updates

---
+++ precache-manifest.acb7f25fb1ef0b4b5b89abdef0059a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd969d95b03d72303ea206d2e71b1aa3",
+    "url": "/to-do-list-2/index.html"
+  },
+  {
+    "revision": "1e906101ad5b0faf103a",
+    "url": "/to-do-list-2/static/css/main.ddf612a4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/to-do-list-2/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1e906101ad5b0faf103a",
+    "url": "/to-do-list-2/static/js/main.cf97d740.chunk.js"
+  },
+  {
+    "revision": "164dead6a915db459800",
+    "url": "/to-do-list-2/static/js/runtime~main.0f7a1323.js"
+  }
+]);
