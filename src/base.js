7a2a380be2d51b0b3b7be63af9a415cd3ed826be
lusterane <tobychow98@gmail.com>
2019-07-27 23:04:49 -0500

    Firebase update

--- src/base.js
+++ src/base.js
@@ -1,15 +1,15 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCTJlU4Ajsdauoo44L8w3yr6CmrOsFq3P8",
-    authDomain: "youdo-81603.firebaseapp.com",
-    databaseURL: "https://youdo-81603.firebaseio.com",
-    projectId: "youdo-81603",
-    storageBucket: "youdo-81603.appspot.com",
-    messagingSenderId: "528940061091",
-    appId: "1:528940061091:web:8cc54fe7785fb5d5"
-  };
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
 
 
 const app = firebase.initializeApp(firebaseConfig)
