a9d85a26a46c49aed886e343b6fb90e5de2c8673
lusterane <tobychow98@gmail.com>
2019-08-03 23:11:52 -0500

    Updates

---
+++ precache-manifest.b935eba0ab95d061d4521cd153786113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61c5fac24490c96a9bee4d5fc4c0660",
+    "url": "/You-Do/index.html"
+  },
+  {
+    "revision": "348f88e00c5ba17d5244",
+    "url": "/You-Do/static/css/main.9cada9e0.chunk.css"
+  },
+  {
+    "revision": "da092042be48badf5283",
+    "url": "/You-Do/static/js/2.58dd9e33.chunk.js"
+  },
+  {
+    "revision": "348f88e00c5ba17d5244",
+    "url": "/You-Do/static/js/main.4127cd97.chunk.js"
+  },
+  {
+    "revision": "527a38b7393acb473e2c",
+    "url": "/You-Do/static/js/runtime~main.653e27aa.js"
+  },
+  {
+    "revision": "c897305473346a639dc77e0c7566bbd9",
+    "url": "/You-Do/static/media/henry-co-tuNGv69BFmM-unsplash.c8973054.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/you-do/precache-manifest.54435b57a64efc58993dc316b3305360.js"
+  "/You-Do/precache-manifest.b935eba0ab95d061d4521cd153786113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/you-do/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/You-Do/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
