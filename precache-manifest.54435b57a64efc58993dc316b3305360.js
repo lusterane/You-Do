d1f45d6ba20266d34133479b0ddedaf67ba8f0b0
lusterane <tobychow98@gmail.com>
2019-08-03 23:03:26 -0500

    Updates

---
+++ precache-manifest.54435b57a64efc58993dc316b3305360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bbc3f47b3cd9690c4208adf18d75a6",
+    "url": "/you-do/index.html"
+  },
+  {
+    "revision": "469a2e4d4950f7a3ce62",
+    "url": "/you-do/static/css/main.99dff013.chunk.css"
+  },
+  {
+    "revision": "da092042be48badf5283",
+    "url": "/you-do/static/js/2.58dd9e33.chunk.js"
+  },
+  {
+    "revision": "469a2e4d4950f7a3ce62",
+    "url": "/you-do/static/js/main.170b7475.chunk.js"
+  },
+  {
+    "revision": "785490a2b67a2badb107",
+    "url": "/you-do/static/js/runtime~main.438f8b24.js"
+  },
+  {
+    "revision": "c897305473346a639dc77e0c7566bbd9",
+    "url": "/you-do/static/media/henry-co-tuNGv69BFmM-unsplash.c8973054.jpg"
+  }
+]);
